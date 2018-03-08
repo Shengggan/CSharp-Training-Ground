1f153e15836aeeddd9d51088e40464766413630b
Sheng Kuang Hou (Shengan) <34660513+Shengggan@users.noreply.github.com>
2018-03-08 04:56:12 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name: ");
+            string inputString = Console.ReadLine();
+            try { 
+                if(inputString.Length > 20) {
+                    throw new Exception ("Input name should not be greater than 20 characters");
+                }
+                Console.WriteLine("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                try {
+                    if(age > 150) {
+                        throw new Exception("Input age can not be above 150");
+                    }
+                    Console.WriteLine("Hello " + inputString + " your age is " + age);
+                    return;
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+        }
+    }
+}
